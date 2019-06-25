e7782c21749a2516405dbaddb303b32a855fec71
Adrià Ciurana Lanau <adri00796@gmail.com>
2019-06-25 23:27:14 +0200

    Update model.py

--- code/model.py
+++ code/model.py
@@ -38,8 +38,6 @@ class Actor(nn.Module):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        
-        
         return F.tanh(self.fc3(x))
 
 
